afd38a18e49613cdad58d1c59fa7f1b4eaeca275
Dima <waverew@gmail.com>
2022-04-27 21:34:52 -0400

    added fs2 assingment

---
+++ fs2_project/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+    
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='Dima'
+			userSecret='sobaka'
+			projectID='6ee445de-c84c-44d2-8791-c4fc212461e8'
+		/>
+	)
+}
+export default App;
